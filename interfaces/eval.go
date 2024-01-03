44652c9b9fb1020679acc401c0b180d7337462ff
Ankush Chavan <cankush625@gmail.com>
2024-01-03 21:55:51 +0530

    feat(interface): Implement expression evaluator using interface
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- interfaces/eval.go
+++ interfaces/eval.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 // An Expr is an arithmetic expression
 type Expr interface {
 	// Eval returns the value of this expr in the environment env
 	Eval(env Env) float64
+	// Check returns non-nil error if expression syntax is invalid
+	Check(vars map[Var]bool) error
 }
 
 // A Var identifies a variable, e.g., x
@@ -18,11 +21,20 @@ func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
 // A literal is a numeric constant, e.g., 4.141
 type literal float64
 
 func (l literal) Eval(_ Env) float64 {
 	return float64(l)
+}
+
+func (literal) Check(_ map[Var]bool) error {
+	return nil
 }
 
 // A unary represents a unary operator expression. e.g., -x
@@ -39,6 +51,13 @@ func (u unary) Eval(env Env) float64 {
 		return -u.x.Eval(env)
 	}
 	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
 }
 
 // A binary represents a binary operator expression, e.g., x+y
@@ -61,6 +80,16 @@ func (b binary) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
 // A call represents a function call expression, e.g., sin(x)
 type call struct {
 	fn   string // one of "pow", "sin", "sqrt"
@@ -78,6 +107,24 @@ func (c call) Eval(env Env) float64 {
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
 
 // To avaluate an Expression containing variables, we will need
 // an environment that maps variable names to values
